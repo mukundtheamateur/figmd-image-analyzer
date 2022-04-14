a4e91ac771f34f7671eacdd61de7424aec96e415
mukundtheamateur <mukundshukla000@gmail.com>
2022-04-14 12:48:49 +0530

    new

---
+++ app.py
@@ -0,0 +1,51 @@
+import cv2
+import pytesseract
+import os
+from PIL import Image
+import sys
+import numpy as np
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import imutils
+from matplotlib import pyplot as pltd
+
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+
+file = open('extract.txt','a') 
+file. truncate(0)
+
+
+def get_string(img_path):
+    # Read image with opencv
+    img = cv2.imread(img_path)
+
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write the image after apply opencv to do some ...
+    cv2.imwrite("bnw.jpg", img)
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open("bnw.jpg"))
+    # os.remove("test1.jpg")
+    
+
+    return result
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv)<2:
+        print("Usage: python app.py relative-filepath")
+    else:
+        print('--- Start recognize text from image ---')
+        for i in range(1,len(argv)):
+            print(argv[i])
+            print(get_string(argv[i]))
+            file.write(get_string(argv[i]))
+
+        print('------ Done -------')
